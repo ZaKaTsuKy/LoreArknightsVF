e36a347c730a5d20c50a7a64801cd34e4742bb39
ZaKaTsuKy <74179251+ZaKaTsuKy@users.noreply.github.com>
2022-06-12 21:03:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -51,42 +51,42 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 //Show & Hide content
-let togg1 = document.getElementById("togg1");
+let style1 = document.getElementById("style");
 let d1 = document.getElementById("d1");
-togg1.addEventListener("click", () => {d1.classList.toggle('d1');})
+style1.addEventListener("click", () => {d1.classList.toggle('d1');})
 
-let togg2 = document.getElementById('togg2');
+let style2 = document.getElementById('style2');
 let d2 = document.getElementById("d2");
-togg2.addEventListener("click", () => {d2.classList.toggle('d2');})
+style2.addEventListener("click", () => {d2.classList.toggle('d2');})
 
-let togg3 = document.getElementById('togg3');
+let style3 = document.getElementById('style3');
 let d3 = document.getElementById("d3");
-togg3.addEventListener("click", () => {d3.classList.toggle('d3');})
+style3.addEventListener("click", () => {d3.classList.toggle('d3');})
 
-let togg4 = document.getElementById('togg4');
+let style4 = document.getElementById('style4');
 let d4 = document.getElementById("d4");
-togg4.addEventListener("click", () => {d4.classList.toggle('d4');})
+style4.addEventListener("click", () => {d4.classList.toggle('d4');})
 
-let togg5 = document.getElementById('togg5');
+let style5 = document.getElementById('style5');
 let d5 = document.getElementById("d5");
-togg5.addEventListener("click", () => {d5.classList.toggle('d5');})
+style5.addEventListener("click", () => {d5.classList.toggle('d5');})
 
-let togg6 = document.getElementById('togg6');
+let style6 = document.getElementById('style6');
 let d6 = document.getElementById("d6");
-togg6.addEventListener("click", () => {d6.classList.toggle('d6');})
+style6.addEventListener("click", () => {d6.classList.toggle('d6');})
 
-let togg7 = document.getElementById('togg7');
+let style7 = document.getElementById('style7');
 let d7 = document.getElementById("d7");
-togg7.addEventListener("click", () => {d7.classList.toggle('d7');})
+style7.addEventListener("click", () => {d7.classList.toggle('d7');})
 
-let togg8 = document.getElementById('togg8');
+let style8 = document.getElementById('style8');
 let d8 = document.getElementById("d8");
-togg8.addEventListener("click", () => {d8.classList.toggle('d8');})
+style8.addEventListener("click", () => {d8.classList.toggle('d8');})
 
-let togg9 = document.getElementById('togg9');
+let style9 = document.getElementById('style9');
 let d9 = document.getElementById("d9");
-togg9.addEventListener("click", () => {d9.classList.toggle('d9');})
+style9.addEventListener("click", () => {d9.classList.toggle('d9');})
 
-let togg10 = document.getElementById('togg10');
+let style10 = document.getElementById('style10');
 let d10 = document.getElementById("d10");
-togg10.addEventListener("click", () => {d10.classList.toggle('d10');})
+style10.addEventListener("click", () => {d10.classList.toggle('d10');})
